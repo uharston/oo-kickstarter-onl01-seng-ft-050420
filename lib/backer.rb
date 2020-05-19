45c33a569be76faaf6ced411521d6a4ba9326775
Uriah Harston <uharston@gmail.com>
2020-05-19 22:31:06 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -9,9 +9,14 @@ class Backer
   end
 
   def back_project(project) #takes project instance as arg.
+    #add project to backer 
     @backed_projects << project
-    binding.pry
-    @backed_projects.title
+    project.backers << self 
+  
+    #add backer to @backer project array 
+    #how to access project @backers array? => project.backers 
+    #add this backer to project.backers.
+    #try self.//// project.backers << self 
   end
 
 end
