cd24a41ff47e3acb9a6be48ce88c8d522d4f4460
Uriah Harston <uharston@gmail.com>
2020-05-19 22:22:15 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,14 +1,14 @@
 class Backer
 
-  attr_reader :backed_projects, :name 
+  attr_reader :backed_projects, :name
 
   def initialize(name)
     @name = name
     @backed_projects = []
   end
 
-  def back_project(project) #takes project instance as arg. 
-    @backed_projects << project 
-  end 
+  def back_project(project) #takes project instance as arg.
+    @backed_projects << project
+  end
 
-end 
+end
